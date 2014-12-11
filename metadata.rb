8d8f7f2bbf0bbd5018f4e78db98386598db88368
Lanvige Jiang <lanvige@gmail.com>
2014-12-11 15:29:38 +0800

    update cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "rails-dependencies-cookbook"
+maintainer        "Lanvige Jiang"
+maintainer_email  "lanvige@gmail.com"
+description       "Installs packages commonly required by rails gems"
+version           "0.1.0"
+
+recipe "rails-dependencies-cookbook", "all the gems"
+
+supports "ubuntu"
---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+package 'python-software-properties'
+# package ubuntu >= 12.10
+# package 'python-software-common'
+
+package 'curl'
+package 'libcurl3'
+package 'libcurl3-dev'
+
+# mysql 2 gem
+package 'libmysql-ruby'
+package 'libmysqlclient-dev'
+
+# package 'libmagickwand-dev'
+# package 'imagemagick'
